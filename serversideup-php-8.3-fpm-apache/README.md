d838b957f9367bb8816ef1a33ff859d6dd90707e
Angus McRitchie <angus@hobbiesdirect.com.au>
2024-10-30 17:44:18 +1100

    added serversideup-php-8.3-fpm-apache

---
+++ serversideup-php-8.3-fpm-apache/README.md
@@ -0,0 +1,11 @@
+## Build the image
+```docker build .```
+
+## Find the image ID
+```docker images```
+
+## Tag the image
+```docker tag <image_id> goolaman/serversideup-php-8.3-fpm-apache-v1.0```
+
+## Push the image
+```docker push goolaman/serversideup-php-8.3-fpm-apache-v1.0```
--- serversideup-php-8.3-fpm-apache/entrypoint.d/20-decrypt-environment-file.sh
+++ serversideup-php-8.3-fpm-apache/entrypoint.d/20-decrypt-environment-file.sh
@@ -12,30 +12,30 @@ fi
 # check APP_BASE_DIR/.env.$APP_ENV file exists, i not skip decryption
 if [ ! -f "$APP_BASE_DIR/.env.$APP_ENV.encrypted" ]; then
     echo "🔐 $APP_BASE_DIR/.env.$APP_ENV.encrypted file not found. Skipping decryption."
-    exit 0
+    return 0
 fi
 
 if [ -f "$APP_BASE_DIR/.env.$APP_ENV" ]; then
     echo "🔐 $APP_BASE_DIR/.env.$APP_ENV file already decrypted. Skipping decryption."
-    exit 0
+    return 0
 fi
 
 # check $LARAVEL_ENV_ENCRYPTION_KEY is set, i not skip decryption
 if [ -z "$LARAVEL_ENV_ENCRYPTION_KEY" ]; then
     echo "🔐 LARAVEL_ENV_ENCRYPTION_KEY not set. Skipping decryption."
-    exit 0
+    return 0
 fi
 
 # check $APP_BASE_DIR/artisan file exists, i not skip decryption
 if [ ! -f "$APP_BASE_DIR/artisan" ]; then
     echo "🔐 Artisan file not found. Skipping decryption."
-    exit 0
+    return 0
 fi
 
 # check environment is not staging or production, then skip decryption
 if [ "$APP_ENV" != "staging" ] && [ "$APP_ENV" != "production" ]; then
     echo "🔐 Skipping decryption for $APP_ENV environment."
-    exit 0
+    return 0
 fi
 
 # lets go
