4be32737411ff2cfad63caeb9bcccb7bcb6e73dc
= <ajmcritchie@gmail.com>
2025-11-22 14:21:42 +1100

    refactor: streamline environment variable checks and improve readability

--- serversideup-php-8.5-fpm-nginx/entrypoint.d/20-decrypt-environment-file.sh
+++ serversideup-php-8.5-fpm-nginx/entrypoint.d/20-decrypt-environment-file.sh
@@ -1,41 +1,38 @@
 #!/bin/sh
 
-if [ -z "$APP_ENV" ]; then
-    APP_ENV="production"
-fi
+# Set default values if not already set
+APP_ENV=${APP_ENV:-production}
+APP_BASE_DIR=${APP_BASE_DIR:-/var/www/html}
 
-# check $APP_BASE_DIR is set, i not set to '/var/www/html'
-if [ -z "$APP_BASE_DIR" ]; then
-    APP_BASE_DIR="/var/www/html"
-fi
-
-# check APP_BASE_DIR/.env.$APP_ENV file exists, i not skip decryption
+# Check if the encrypted environment file exists
 if [ ! -f "$APP_BASE_DIR/.env.$APP_ENV.encrypted" ]; then
     echo "🔐 $APP_BASE_DIR/.env.$APP_ENV.encrypted file not found. Skipping decryption."
-    return 0
+    exit 0
 fi
 
+# Check if the environment file is already decrypted
 if [ -f "$APP_BASE_DIR/.env.$APP_ENV" ]; then
     echo "🔐 $APP_BASE_DIR/.env.$APP_ENV file already decrypted. Skipping decryption."
-    return 0
+    exit 0
 fi
 
-# check $LARAVEL_ENV_ENCRYPTION_KEY is set, i not skip decryption
+# Check if the encryption key is set
 if [ -z "$LARAVEL_ENV_ENCRYPTION_KEY" ]; then
     echo "🔐 LARAVEL_ENV_ENCRYPTION_KEY not set. Skipping decryption."
-    return 0
+    exit 0
 fi
 
-# The artisan file must exist to run decryption
+# Check if the artisan file exists
 if [ ! -f "$APP_BASE_DIR/artisan" ]; then
     echo "🔐 Artisan file not found. Skipping decryption."
-    return 0
+    exit 0
 fi
 
+# Attempt to decrypt the environment file
 if grep -q "APP_KEY" "$APP_BASE_DIR/.env.$APP_ENV.encrypted"; then
-    echo "ℹ️ $APP_BASE_DIR/.env.$APP_ENV.encrypted contains APP_KEY variable, so attempting to decrypting values only."
-    php "$APP_BASE_DIR/artisan" env:decrypt --env=$APP_ENV --only-values
+    echo "ℹ️ $APP_BASE_DIR/.env.$APP_ENV.encrypted contains APP_KEY variable. Attempting to decrypt values only."
+    php "$APP_BASE_DIR/artisan" env:decrypt --env="$APP_ENV" --only-values
 else
     echo "🔐 Decrypting $APP_BASE_DIR/.env.$APP_ENV.encrypted file..."
-    php "$APP_BASE_DIR/artisan" env:decrypt --env=$APP_ENV
+    php "$APP_BASE_DIR/artisan" env:decrypt --env="$APP_ENV"
 fi
